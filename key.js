572853f99283af488bea6720324f3c56be61c513
geethu4365 <132776454+geethu4365@users.noreply.github.com>
2024-07-27 18:00:41 +0530

    Add files via upload

---
+++ key.js
@@ -0,0 +1,4 @@
+key="6b9b45681a357a35887ee5a234e6ba67";
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
